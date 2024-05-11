d5f793bb0f234c99700859537e817e71955f93c8
Lior Dvir <liordvir@campus.technion.ac.il>
2024-05-11 12:50:33 +0300

    Added Edited tennis pics ans masks

--- Matlab/Lior_Neta_main.m
+++ Matlab/Lior_Neta_main.m
@@ -8,6 +8,7 @@ addpath ./code
 addpath ./data
 addpath ./admm
 addpath ./Images
+addpath ../tennis_ball_frames
 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -86,8 +87,8 @@ lambda     = 3;
 mode       = 'sotv';
 padNum     = 8;
 
-numImages = 2;
-imRef = double(imread('Banana_edit_3.jpg'));
+numImages = 5;
+imRef = double(imread('Banana_edit_1.jpg'));
 imRef = rescale_intensity(imRef(:,:,1), [1, 99]);
 imRef = padarray(imRef,  [padNum,padNum], 0);
 
@@ -95,19 +96,19 @@ maskRefCells = cell(numImages,1);
 maskCurCells = cell(numImages,1);
 iouCells = cell(numImages,1);
 
-maskRefCells{1} = double(imread('Banana_mask_edit_3.jpg'));
+maskRefCells{1} = double(imread('Banana_mask_edit_1.jpg'));
 maskRefCells{1} = padarray(maskRefCells{1},  [padNum,padNum], 0);
 maskCurCells{1} = maskRefCells{1};
 iouCells{1} = calcIOU(maskRefCells{1}, maskCurCells{1}); % Should be 1
 
 for i=2:numImages
     % Load current image
-    imCur = double(imread('Banana_edit_'+string(i+2)+'.jpg'));
+    imCur = double(imread('Banana_edit_'+string(i)+'.jpg'));
     imCur = rescale_intensity(imCur(:,:,1), [1, 99]);
     imCur = padarray(imCur,  [padNum,padNum], 0);
 
     % Load current image mask
-    maskCur = double(imread('Banana_mask_edit_'+string(i+2)+'.jpg'));
+    maskCur = double(imread('Banana_mask_edit_'+string(i)+'.jpg'));
     maskCur = padarray(maskCur,  [padNum,padNum], 0);
 
     % Calculate displacement field
@@ -126,6 +127,66 @@ end
 figure
 for i=1:numImages
     subplot(1,5,i)
+    imshowpair(maskRefCells{i}, maskCurCells{i})
+    title('IOU = ' + string(iouCells{i}));
+end
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Tennis Images
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%source: moving image
+%target: fixed image
+
+levels     = [4,2,1];
+maxIter    = 2000;
+tolerance  = 1e-2;
+difference = 2;
+talyor     = 5;
+lambda     = 1;
+mode       = 'sotv';
+padNum     = 8;
+
+numImages = 10;
+imRef = double(imread('Tennis_edit_1.jpg'));
+imRef = rescale_intensity(imRef(:,:,1), [1, 99]);
+imRef = padarray(imRef,  [padNum,padNum], 0);
+
+maskRefCells = cell(numImages,1);
+maskCurCells = cell(numImages,1);
+iouCells = cell(numImages,1);
+
+maskRefCells{1} = double(imread('Tennis_mask_edit_1.jpg'));
+maskRefCells{1} = padarray(maskRefCells{1},  [padNum,padNum], 0);
+maskCurCells{1} = maskRefCells{1};
+iouCells{1} = calcIOU(maskRefCells{1}, maskCurCells{1}); % Should be 1
+
+for i=2:numImages
+    % Load current image
+    imCur = double(imread('Tennis_edit_'+string(i)+'.jpg'));
+    imCur = rescale_intensity(imCur(:,:,1), [1, 99]);
+    imCur = padarray(imCur,  [padNum,padNum], 0);
+
+    % Load current image mask
+    maskCur = double(imread('Tennis_mask_edit_'+string(i)+'.jpg'));
+    maskCur = padarray(maskCur,  [padNum,padNum], 0);
+
+    % Calculate displacement field
+    [u0, v0] = pyramid_flow(imRef, imCur, levels, talyor, maxIter, lambda, tolerance, difference, mode);
+
+    % warp mask using displacement field and calculate IOU between masks
+    maskRefTemp = imwarp(maskRefCells{i-1}, cat(3, u0, v0),'Interp', 'linear'); 
+    maskRefTemp(maskRefTemp <= 0) = 0;
+    maskRefCells{i} = maskRefTemp;
+    maskCurCells{i} = maskCur;
+    iouCells{i} = calcIOU(maskRefCells{i}, maskCurCells{i});
+
+    imRef = imCur;
+end
+
+figure
+for i=1:numImages
+    subplot(2,5,i)
     imshowpair(maskRefCells{i}, maskCurCells{i})
     title('IOU = ' + string(iouCells{i}));
 end
